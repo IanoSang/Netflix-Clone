eb39455a8cfda187cb04b9484f6bccdfba9da7a2
IanoSang <ianosang18@gmail.com>
2022-06-02 02:08:05 +0300

    patch: adds the django project and the neflix app files and folders

---
+++ app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, re_path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
